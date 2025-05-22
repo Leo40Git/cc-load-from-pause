6fa993b96b81ce23fa5086bcf730234f781c4187
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2025-05-22 20:54:45 +0300

    Fix compatibility with Cheats mod

--- prestart.js
+++ prestart.js
@@ -33,36 +33,31 @@ ig.module('load-from-pause')
                 } else {
                     this.addChildGui(this.loadGameButton);
                     
-                    // "Load Game" goes between "Save Game" and "Return to Title"
-                    // we need to reposition and reassign focus indices...
-                    this.buttonGroup.clear();
+                    // "Load Game" button goes between "Save Game" and "Return to Title"
+                    // insert it after "Save Game" in keyboard order,
+                    // pushing everyone else down
+                    this.buttonGroup.insertFocusGui(this.loadGameButton, 0, 2);
                     
-                    this.buttonGroup.addFocusGui(this.resumeButton, 0, 0, true);
-                    this.buttonGroup.addFocusGui(this.optionsButton, 0, 1);
-                    this.buttonGroup.addFocusGui(this.loadGameButton, 0, 2);
-                    this.buttonGroup.addFocusGui(this.saveGameButton, 0, 3);
-                    
+                    // now we need to reposition everything...
+                    // there's probably a better way to do this that's more compatible
+                    // with other mods that add buttons to the pause menu,
+                    // but AFAIK there's only 2 mods that do so, and we're one of them.
                     this.toTitleButton.setPos(3, 3);
                     c = this.toTitleButton.hook.size.y;
                     
                     if (sc.model.currentState == sc.GAME_MODEL_STATE.CUTSCENE && !sc.model.skipBlock) {
-                        this.buttonGroup.addFocusGui(this.skipButton, 0, 4);
-                        this.buttonGroup.addFocusGui(this.toTitleButton, 0, 5);
                         this.skipButton.setPos(3, 3 + c + 4);
                         this.saveGameButton.setPos(3, c * 2 + 11);
                         this.loadGameButton.setPos(3, c * 3 + 15);
                         this.optionsButton.setPos(3, c * 4 + 19);
                         this.resumeButton.setPos(3, c * 5 + 23)
                     } else if (sc.model.cancelButtonText) {
-                        this.buttonGroup.addFocusGui(this.cancelButton, 0, 4);
-                        this.buttonGroup.addFocusGui(this.toTitleButton, 0, 5);
                         this.cancelButton.setPos(3, 3 + c + 4);
                         this.saveGameButton.setPos(3, c * 2 + 11);
                         this.loadGameButton.setPos(3, c * 3 + 15);
                         this.optionsButton.setPos(3, c * 4 + 19);
                         this.resumeButton.setPos(3, c * 5 + 23)
                     } else {
-                        this.buttonGroup.addFocusGui(this.toTitleButton, 0, 4);
                         this.saveGameButton.setPos(3, 3 + c + 4);
                         this.loadGameButton.setPos(3, c * 2 + 11);
                         this.optionsButton.setPos(3, c * 3 + 15);
@@ -75,6 +70,15 @@ ig.module('load-from-pause')
                         this.loadGameButton.setText(c);
                         this.loadGameButton.setWidth(sc.BUTTON_DEFAULT_WIDTH)
                     } else this.loadGameButton.setActive(sc.model.isSaveAllowed())
+                    
+                    // COMPAT with Cheats mod
+                    if (this.cheatsButton) {
+                        // get the first button 
+                        const firstButtonHook = this.buttonGroup.elements[0][0].hook;
+                        // position the "Cheats" button above everyone else
+                        this.cheatsButton.setPos(firstButtonHook.pos.x, firstButtonHook.pos.y + firstButtonHook.size.y + 16);
+                    }
+                    // END COMPAT with Cheats mod
                 }
             }
         });
