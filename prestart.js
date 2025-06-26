5d4d1137ef637410a7feb43d178127d1e36023f0
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2025-06-26 15:06:54 +0300

    Restore show/enable conditions

--- prestart.js
+++ prestart.js
@@ -1,12 +1,21 @@
 nax.ccuilib.pauseScreen.addButton({
     text: '',
+    showCondition() {
+        // replicate "Save Game" button conditions
+        var c = sc.arena.runtime;
+        return !(sc.arena.active && c && c.roundStarted && !c.roundFinished && !c.roundEndPre && sc.model.currentState != sc.GAME_MODEL_STATE.CUTSCENE)
+    },
+    enabledCondition() {
+        // replicate "Save Game" button conditions
+        return sc.model.isSaveAllowed()
+    },
+    onShow(button) {
+        button.setText(ig.lang.get('sc.gui.title-screen.load'), true)
+    },
     onPress() {
         sc.menu.loadMode = true;
         sc.menu.setDirectMode(true, sc.MENU_SUBMENU.SAVE);
         sc.model.enterMenu(true)
-    },
-    onShow(button) {
-        button.setText(ig.lang.get('sc.gui.title-screen.load'), true)
     }
 })
 
